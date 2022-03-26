cc96b86fce5cbb16f69c20f1256441e52fa2db4f
thwonp <acsiracuse@gmail.com>
2022-03-26 16:24:54 -0400

    Update comments

--- lpl-helper.sh
+++ lpl-helper.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
+
+# A script for generating Retroach playlist files by scanning ROMs.
+
 IFS=$'\n'
 
 function generate_lpls {
   # Parent directory of roms
   # Sub-directories are used as playlist name
-  rom_parent_dir="ux0:/data/retroarch/roms" # VITA
-
-  # output directory
-  odir="tmp_thumbs"
-
+  rom_parent_dir="ux0:/data/retroarch/roms" # Vita
+  # rom_parent_dir="C:\\RetroArch-Win64\\roms\\" # Windows
   rom_dir="roms"
 
-  ## DANGER ##
+  # Output directory
+  odir="tmp_thumbs"
+
+  # Start fresh with a clean output directory.
   rm -rf ${odir}/
   mkdir -p "${odir}"
 
   for dir in $(find ${rom_dir}/* -type d); do
-
+  # Scan sub-directories to generate a unique playlist for each system.
     playlist_name="${dir#*/}"
     touch "${odir}/${playlist_name}.lpl"
     cat <<EOF > "${odir}/${playlist_name}.lpl"
@@ -25,8 +28,8 @@ function generate_lpls {
   "items": [
 EOF
 
-    # Iterate over all files
     for file in $(ls ${rom_dir}/${playlist_name}/) ; do
+      # Iterate over all rom files and add it to the playlist.
       rom_label="${file%.*}"
       cat <<EOF >> "${odir}/${playlist_name}.lpl"
     {
@@ -39,7 +42,8 @@ EOF
     },
 EOF
     done
-    # Remove final comma. This way is janky but preserves permissions
+    # Remove final comma from the json. 
+    # This method is janky but allows permissions to be preserved.
     sed '$d' "${odir}/${playlist_name}.lpl" > ${odir}/tmp_file.tmp
     mv "${odir}/tmp_file.tmp" "${odir}/${playlist_name}.lpl"
     cat <<EOF >> "${odir}/${playlist_name}.lpl"
@@ -53,23 +57,18 @@ EOF
 
 
 function clean_thumb_names {
-  # Illegal characters for thumbnail names
+  # Strips illegal characters from thumbnails by renaming .png files.
+
   CHARS=('&' '*' '/' ':' '`' '<' '>' '?' '\' '|')
   
-  # Iterate over all files
-  for file in ${DIR}/* ; do
-
-    # Substring to remove leading directory
-    file=${file#${DIR}/}
-
-    # Find png files
+  for file in ${DIR}/* ; do    
+    file=${file#${DIR}/} # Substring to remove leading directory
+  
     if $(grep -q ".png" <<<"${file}"); then
       thumb="${file}"
-      # Check if the png filename contains an illegal character
+      
       for illegal_char in "${CHARS[@]}"; do
         if grep -q "${illegal_char}" <<<"${thumb}"; then
-
-        # Substring search/replace illegal character
           thumb_clean="${thumb//${illegal_char}/_}"
           echo "INFO: Illegal char ${illegal_char} found in $thumb"
           echo "INFO: Renaming to: $thumb_clean"
@@ -79,3 +78,10 @@ function clean_thumb_names {
     fi
   done
 }
+
+# TODO: getopts function
+
+generate_lpls
+
+# chdman createcd -i "Policenauts (Japan) (Disc 2) [En by Slowbeef v1.0].cue" -o "Policenauts - Eng. Patch - Disc 2.chd"
+# xargs -P 10 -n 1 curl -O -L < ../psxurls
