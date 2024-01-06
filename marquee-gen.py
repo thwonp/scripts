876e689df4ed864f0db6664c2a014d8873940ac1
thwonp <acsiracuse@gmail.com>
2024-01-05 23:42:17 -0500

    Update marquee-gen.py

--- marquee-gen.py
+++ marquee-gen.py
@@ -1,5 +1,4 @@
 from wand.image import Image
-from wand.drawing import Drawing
 from wand.font import Font
 from wand.color import Color
   
@@ -8,17 +7,16 @@ w, h = 800, 350
 with Image(width = w, 
             height = h, 
             background = Color('none')) as canvas: 
-    with Drawing() as content:
-        font = Font('spiritendo.otf', 
-                    size=0,  
-                    color="darkred",
-                    stroke_color="white",
-                    stroke_width=4
-                    )
-        canvas.caption('Hello World', 
-                       width=w, 
-                       height=h, 
-                       font=font,   
-                       gravity='center') 
+    font = Font('spiritendo.otf', 
+                size=0,  
+                color="darkred",
+                stroke_color="white",
+                stroke_width=4
+                )
+    canvas.caption('Hello World', 
+                    width=w, 
+                    height=h, 
+                    font=font,   
+                    gravity='center') 
     canvas.trim()
     canvas.save(filename='clear_logo.png')
