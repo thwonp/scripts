dad58f6e00c25ccab99e03591cd04811c70d19f6
thwonp <acsiracuse@gmail.com>
2025-06-23 10:46:47 -0400

    Create ps3-squash.sh

---
+++ ps3-squash.sh
@@ -0,0 +1,14 @@
+for file in *.iso; do
+	name=$(basename "${file}")
+	mount -o loop "${name}" tmpmnt/
+	mkdir "${name/iso/ps3}"
+	echo "Copying data for ${name/.iso/}"
+	cp -rv tmpmnt/* "${name/iso/ps3}"/
+	echo "Unmounting..."
+	umount "${name}"
+	echo "Squashing ${name/iso/}.."
+	mksquashfs "${name/iso/ps3}" "${name/iso/ps3.squashfs}" -comp xz
+	echo "Cleaning up..."
+	rm -rf "${name/iso/ps3}"
+	echo "Done"
+done
